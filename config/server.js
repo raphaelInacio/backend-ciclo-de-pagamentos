91f27636b79071a74d3378379db8e2366a56e896
Raphael <contato.raphaelinacio@gmail.com>
2017-06-15 23:26:25 -0300

    parser

--- config/server.js
+++ config/server.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const cors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended : true }))
 server.use(bodyParser.json())
 server.use(cors)
+server.use(queryParser())
 
 server.listen(port , function() {
     console.log(`Backend is runing on port ${port}`)
